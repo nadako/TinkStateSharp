19fcd9627441be4d6991924d4ee9902a7e46e612
Dan Korostelev <nadako@gmail.com>
2022-12-10 17:36:21 +0100

    Write some documentation.

--- src/TinkState-Model/Runtime/Attributes.cs
+++ src/TinkState-Model/Runtime/Attributes.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace TinkState.Model
 {
+	/// <summary>
+	/// Use Observable or State backing field for the property.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Property)]
 	public class ObservableAttribute : Attribute {}
 }
--- src/TinkState-Model/Runtime/Model.cs
+++ src/TinkState-Model/Runtime/Model.cs
@@ -3,6 +3,9 @@ using System.Linq.Expressions;
 
 namespace TinkState.Model
 {
+	/// <summary>
+	/// Enable support for the <see cref="ObservableAttribute">[Observable]</see> attribute on properties of the implementing class.
+	/// </summary>
 	public interface Model { }
 
 	interface ModelInternal
@@ -10,8 +13,19 @@ namespace TinkState.Model
 		Observable<T> GetObservable<T>(string field);
 	}
 
+	/// <summary>
+	/// Extension methods for implementors of the <see cref="Model"/> interface.
+	/// </summary>
 	public static class ModelExtensions
 	{
+		/// <summary>
+		/// Get the underlying backing <see cref="Observable{T}"/> object for given property with the <see cref="ObservableAttribute">[Observable]</see> attribute.
+		/// </summary>
+		/// <param name="model">Model class instance</param>
+		/// <param name="expr">Lambda expression in form of <c>_ => c.Property</c></param>
+		/// <typeparam name="M">Type of the model to retrieve an observable from.</typeparam>
+		/// <typeparam name="T">Type of the property to retrieve an observable for.</typeparam>
+		/// <returns>Backing <see cref="Observable{T}"/> object for given property.</returns>
 		public static Observable<T> GetObservable<M, T>(this M model, Expression<Func<M, T>> expr) where M : Model
 		{
 			var memberExpr = expr.Body as MemberExpression;
@@ -21,6 +35,16 @@ namespace TinkState.Model
 			return GetObservable<T>(model, fieldName);
 		}
 
+		/// <summary>
+		/// Get the underlying backing <see cref="Observable{T}"/> object for given property with the <see cref="ObservableAttribute">[Observable]</see> attribute.
+		/// </summary>
+		/// <remarks>
+		/// This method is not type safe, prefer <see cref="GetObservable{M,T}"/> instead.
+		/// </remarks>
+		/// <param name="model">Model class instance</param>
+		/// <param name="field">Name of the string</param>
+		/// <typeparam name="T">Type of the property to retrieve an observable for.</typeparam>
+		/// <returns>Backing <see cref="Observable{T}"/> object for given property.</returns>
 		public static Observable<T> GetObservable<T>(this Model model, string field)
 		{
 			return ((ModelInternal)model).GetObservable<T>(field);
