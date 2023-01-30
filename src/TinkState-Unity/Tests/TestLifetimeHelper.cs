a9b4e843c8d38557930a2b0c02a54f69a386f5b8
Dan Korostelev <nadako@gmail.com>
2023-01-30 12:04:14 +0100

    Add tests for RunOnActive and ClearOnActiveRuns

--- src/TinkState-Unity/Tests/TestLifetimeHelper.cs
+++ src/TinkState-Unity/Tests/TestLifetimeHelper.cs
@@ -72,9 +72,101 @@ public class TestLifetimeHelper
 		GameObject.Destroy(go);
 		yield return null; // Destroy is delayed, so wait a frame
 
+		// shouldn't fail
+		go.ClearOnDestroyDisposes();
+
 		// still 1
 		Assert.That(dispose1Calls, Is.EqualTo(1));
 		Assert.That(dispose2Calls, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void TestRunOnActive()
+	{
+		var go = new GameObject();
+		var runs = 0;
+		var disposes = 0;
+		go.RunOnActive(() =>
+		{
+			runs++;
+			return new CallbackDisposable(() => disposes++);
+		});
+
+		Assert.That(runs, Is.EqualTo(1));
+		Assert.That(disposes, Is.EqualTo(0));
+
+		go.SetActive(false);
+		Assert.That(runs, Is.EqualTo(1));
+		Assert.That(disposes, Is.EqualTo(1));
+
+		go.SetActive(true);
+		Assert.That(runs, Is.EqualTo(2));
+		Assert.That(disposes, Is.EqualTo(1));
+
+		go.SetActive(false);
+		Assert.That(runs, Is.EqualTo(2));
+		Assert.That(disposes, Is.EqualTo(2));
+	}
+
+	[Test]
+	public void TestRunOnActiveWhenInactive()
+	{
+		var go = new GameObject();
+		go.SetActive(false);
+		var runs = 0;
+		var disposes = 0;
+		go.RunOnActive(() =>
+		{
+			runs++;
+			return new CallbackDisposable(() => disposes++);
+		});
+
+		Assert.That(runs, Is.EqualTo(0));
+		Assert.That(disposes, Is.EqualTo(0));
+
+		go.SetActive(true);
+		go.SetActive(false);
+		Assert.That(runs, Is.EqualTo(1));
+		Assert.That(disposes, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void TestClearOnActiveRuns()
+	{
+		var go = new GameObject();
+		var runs1 = 0;
+		var runs2 = 0;
+		var disposes1 = 0;
+		var disposes2 = 0;
+		go.RunOnActive(() =>
+		{
+			runs1++;
+			return new CallbackDisposable(() => disposes1++);
+		});
+		go.RunOnActive(() =>
+		{
+			runs2++;
+			return new CallbackDisposable(() => disposes2++);
+		});
+
+		Assert.That(runs1, Is.EqualTo(1));
+		Assert.That(disposes1, Is.EqualTo(0));
+		Assert.That(runs2, Is.EqualTo(1));
+		Assert.That(disposes2, Is.EqualTo(0));
+
+		go.ClearOnActiveRuns();
+		Assert.That(runs1, Is.EqualTo(1));
+		Assert.That(disposes1, Is.EqualTo(1));
+		Assert.That(runs2, Is.EqualTo(1));
+		Assert.That(disposes2, Is.EqualTo(1));
+
+		go.ClearOnActiveRuns();
+		go.SetActive(true);
+		go.SetActive(false);
+		Assert.That(runs1, Is.EqualTo(1));
+		Assert.That(disposes1, Is.EqualTo(1));
+		Assert.That(runs2, Is.EqualTo(1));
+		Assert.That(disposes2, Is.EqualTo(1));
 	}
 
 	class CallbackDisposable : IDisposable
