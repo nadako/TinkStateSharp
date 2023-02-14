a88a1dbd34b9c3031818239922812829c67d3d92
Dan Korostelev <nadako@gmail.com>
2023-02-14 11:53:25 +0100

    Add ObservableList.Observe method to expose list entries as an Observable<IReadOnlyList<T>> (see #5)

---
+++ src/TinkState/Runtime/Internal/ObservableList_ObservableOfReadOnlyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TinkState.Internal
+{
+	partial class ObservableList<T> : Observable<IReadOnlyList<T>>, DispatchingObservable<IReadOnlyList<T>>
+	{
+		IReadOnlyList<T> Observable<IReadOnlyList<T>>.Value
+		{
+			get => AutoObservable.Track<IReadOnlyList<T>>(this);
+		}
+
+		IDisposable Observable<IReadOnlyList<T>>.Bind(Action<IReadOnlyList<T>> callback, IEqualityComparer<IReadOnlyList<T>> comparer, Scheduler scheduler)
+		{
+			return new Binding<IReadOnlyList<T>>(this, callback, comparer, scheduler);
+		}
+
+		Observable<TOut> Observable<IReadOnlyList<T>>.Map<TOut>(Func<IReadOnlyList<T>, TOut> transform, IEqualityComparer<TOut> comparer)
+		{
+			return new TransformObservable<IReadOnlyList<T>, TOut>(this, transform, comparer);
+		}
+
+		IReadOnlyList<T> ValueProvider<IReadOnlyList<T>>.GetCurrentValue()
+		{
+			valid = true;
+			return entries;
+		}
+
+		IEqualityComparer<IReadOnlyList<T>> DispatchingObservable<IReadOnlyList<T>>.GetComparer()
+		{
+			return NeverEqualityComparer<IReadOnlyList<T>>.Instance;
+		}
+	}
+}
