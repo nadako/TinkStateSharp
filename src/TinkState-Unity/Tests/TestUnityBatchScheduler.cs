7f1c09610e6ddc9d5a2246080789f605af0e5f3a
Dan Korostelev <nadako@gmail.com>
2023-06-12 17:43:40 +0200

    Swap between two List for the UnityBatchScheduler queues instead of allocating a new one each time (closes #19)
    Also add tests for the case of one binding scheduling another

--- src/TinkState-Unity/Tests/TestUnityBatchScheduler.cs
+++ src/TinkState-Unity/Tests/TestUnityBatchScheduler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using NUnit.Framework;
 using UnityEngine.TestTools;
 using TinkState;
+using UnityEngine;
 
 public class TestUnityBatchScheduler
 {
@@ -63,5 +64,44 @@ public class TestUnityBatchScheduler
         Assert.That(bindingCalls, Is.EqualTo(1));
         yield return null;
         Assert.That(bindingCalls, Is.EqualTo(1));
+    }
+
+    // NOTE: this is not an intended usage for bindings, as one shouldn't
+    // trigger bindings from other bindings (e.g. by changing a State) and
+    // instead make use of auto-Observables and such, but we still test
+    // that this works as expected and doesn't hang
+    [UnityTest]
+    public IEnumerator TestTriggerBindingFromBinding()
+    {
+	    var s = Observable.State(2);
+	    var s2 = Observable.State(2);
+
+	    var bindingCalls = 0;
+	    var expectedValue = 2;
+	    void BindCallback(int value)
+	    {
+		    bindingCalls++;
+		    Assert.That(value, Is.EqualTo(expectedValue));
+	    }
+
+	    s2.Bind(BindCallback);
+	    Assert.That(bindingCalls, Is.EqualTo(1));
+
+	    s.Bind(v => s2.Value = v * 2);
+
+	    // first binding was still not called because it's scheduled!
+	    Assert.That(bindingCalls, Is.EqualTo(1));
+
+	    // it will be called as scheduled though
+	    expectedValue = 4;
+	    yield return null;
+	    Assert.That(bindingCalls, Is.EqualTo(2));
+
+	    // changing the value will invoke the binding for `s` which will in its turn schedule the binding for `s2`,
+	    // which should be processed right after, let's check that
+	    s.Value = 3;
+	    expectedValue = 6;
+	    yield return null;
+	    Assert.That(bindingCalls, Is.EqualTo(3));
     }
 }
---
+++ src/TinkState/Tests/TestTriggerBindingFromBinding.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using TinkState;
+
+namespace Test
+{
+	// NOTE: this is not an intended usage for bindings, as one shouldn't
+	// trigger bindings from other bindings (e.g. by changing a State) and
+	// instead make use of auto-Observables and such, but we still test
+	// that this works as expected and doesn't hang
+	class TestTriggerBindingFromBinding : BaseTest
+	{
+		[Test]
+		public void Test()
+		{
+			var s = Observable.State(2);
+			var s2 = Observable.State(2);
+
+			var bindingCalls = 0;
+			var expectedValue = 2;
+			void BindCallback(int value)
+			{
+				bindingCalls++;
+				Assert.That(value, Is.EqualTo(expectedValue));
+			}
+
+			s2.Bind(BindCallback);
+			Assert.That(bindingCalls, Is.EqualTo(1));
+
+			expectedValue = 4;
+			s.Bind(v => s2.Value = v * 2);
+			Assert.That(bindingCalls, Is.EqualTo(2));
+
+			expectedValue = 6;
+			s.Value = 3;
+			Assert.That(bindingCalls, Is.EqualTo(3));
+		}
+	}
+}
